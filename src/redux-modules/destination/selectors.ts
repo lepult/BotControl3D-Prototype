5a3b9678ce77617ac59004e0d8e148cfaea53d42
Leopold Pinkernell <leopold.pinkernell@tobit.com>
2024-02-13 10:52:59 +0100

    :sparkles: Add dialogs to change robot settings

--- src/redux-modules/destination/selectors.ts
+++ src/redux-modules/destination/selectors.ts
@@ -1,6 +1,19 @@
 import { RootState } from '../index';
+import { createSelector } from '@reduxjs/toolkit';
+import { CustomDestinationType } from '../../types/api/destination';
 
 export const selectDestinationIds = (state: RootState) => state.destination.ids;
 export const selectDestinationIdsByMapId = (mapId: number) => (state: RootState) => state.destination.idsByMapId[mapId];
 export const selectDestinationById = (mapId: number) => (state: RootState) => state.destination.entities[mapId];
 export const selectDestinationEntities = (state: RootState) => state.destination.entities;
+
+export const selectDestinationsByCustomType = createSelector(
+    [
+        (state: RootState) => state.destination,
+        (_: RootState, customType: CustomDestinationType) => customType,
+    ],
+    (
+        destination,
+        customType
+    ) => destination.idsByCustomType[customType].map((id) => destination.entities[id]),
+);
