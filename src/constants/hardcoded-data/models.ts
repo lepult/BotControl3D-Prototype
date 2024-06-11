81a53dee304d2957911ff5098cd4595f8bf6ecb7
Leopold Pinkernell <leopold.pinkernell@tobit.com>
2024-06-11 11:19:10 +0200

    Add T1 EG Models

--- src/constants/hardcoded-data/models.ts
+++ src/constants/hardcoded-data/models.ts
@@ -7,17 +7,35 @@ export type ModelType = {
 
 
 
-export const floorModelsEg: ModelType[] = [{
-    id: "EG/Dome",
-    url: "https://chayns.space/77896-05853/3D-Modelle/EG/Dome.glb",
-    position: [0.30353255969106474, -116.77306274331785, 0],
-    orientation: [0, -50.97685956018226, 90]
-}, {
-    id: "EG/Atrium_Norden",
-    url: "https://chayns.space/77896-05853/3D-Modelle/EG/Atrium_Norden.glb",
-    position: [6.546039244784711, -79.53639843228481, 0],
-    orientation: [0, 206.6837556318823, 90]
-},
+export const floorModelsEg: ModelType[] = [
+    {
+        "id": "EG/Dome",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/Dome.glb",
+        "position": [
+            0.30353255969106474,
+            -116.77306274331785,
+            0
+        ],
+        "orientation": [
+            0,
+            -50.97685956018226,
+            90
+        ]
+    },
+    {
+        "id": "EG/Atrium_Norden",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/Atrium_Norden.glb",
+        "position": [
+            6.546039244784711,
+            -79.53639843228481,
+            0
+        ],
+        "orientation": [
+            0,
+            206.6837556318823,
+            90
+        ]
+    },
     {
         "id": "EG/Dome",
         "url": "https://chayns.space/77896-05853/3D-Modelle/EG/Dome.glb",
@@ -113,6 +131,160 @@ export const floorModelsEg: ModelType[] = [{
         "orientation": [
             0,
             -93.30179866558458,
+            90
+        ]
+    },
+    {
+        "id": "EG/Garderobe",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_7.glb",
+        "position": [
+            8.37262972296685,
+            -7.484942001631906,
+            0
+        ],
+        "orientation": [
+            0,
+            443.96840398896063,
+            90
+        ]
+    },
+    {
+        "id": "EG/Wand1",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_8.glb",
+        "position": [
+            7.8994221563348175,
+            -5.927570440347693,
+            0
+        ],
+        "orientation": [
+            0,
+            -184.24892687375515,
+            90
+        ]
+    },
+    {
+        "id": "EG/Shop",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_10.glb",
+        "position": [
+            0.04383008329715117,
+            4.173118758960468,
+            -0.1
+        ],
+        "orientation": [
+            0,
+            75.85279464235711,
+            90
+        ]
+    },
+    {
+        "id": "EG/Lounge1",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_11.glb",
+        "position": [
+            9.261361967099832,
+            15.016220351248633,
+            0
+        ],
+        "orientation": [
+            0,
+            -15.763297893624411,
+            90
+        ]
+    },
+    {
+        "id": "EG/Lounge2",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_12.glb",
+        "position": [
+            15.631595093981895,
+            10.81326371910401,
+            0
+        ],
+        "orientation": [
+            0,
+            -111.38908052664442,
+            90
+        ]
+    },
+    {
+        "id": "EG/M7",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_13.glb",
+        "position": [
+            -2.170760260228348,
+            -6.2242431992838,
+            0
+        ],
+        "orientation": [
+            0,
+            85.52420203901987,
+            90
+        ]
+    },
+    {
+        "id": "EG/M5",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_14.glb",
+        "position": [
+            -2.506788033987528,
+            -19.830243854297308,
+            0
+        ],
+        "orientation": [
+            0,
+            85.54075948021853,
+            90
+        ]
+    },
+    {
+        "id": "EG/Brücke",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_15.glb",
+        "position": [
+            7.696993157174157,
+            7.424506315491648,
+            0
+        ],
+        "orientation": [
+            0,
+            -109.07759025283346,
+            90
+        ]
+    },
+    {
+        "id": "EG/Haupteingang",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_16.glb",
+        "position": [
+            18.806009532763788,
+            3.4893142655063794,
+            0
+        ],
+        "orientation": [
+            0,
+            -19.224841248022784,
+            90
+        ]
+    },
+    {
+        "id": "EG/FlurAußen",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_17.glb",
+        "position": [
+            17.937836082335526,
+            -10.152446417073673,
+            0
+        ],
+        "orientation": [
+            0,
+            -1.828196701345476,
+            90
+        ]
+    },
+    {
+        "id": "EG/Flur2",
+        "url": "https://chayns.space/77896-05853/3D-Modelle/EG/compressed_18.glb",
+        "position": [
+            1.3189141779978852,
+            -10.294067066995087,
+            0
+        ],
+        "orientation": [
+            0,
+            -183.29598324773255,
             90
         ]
     }
